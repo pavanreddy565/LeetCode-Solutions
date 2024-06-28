9a9ca889ffc56c70a7d831d846352885137413f0
Pavan Reddy Satti <118699787+pavanreddy565@users.noreply.github.com>
2024-06-28 10:25:45 +0530

    Time: 43 ms (33.01%), Space: 16.6 MB (16.13%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,6 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        l=m-1
+        r=0
+        while(l>=0 and r<n and nums1[l]>nums2[r]):
+            swap=nums1[l]
+            nums1[l]=nums2[r]
+            nums2[r]=swap
+            l-=1
+            r+=1
+            print(nums1)
+        print(nums1,nums2)
+        
+        
         for i in range(m,n+m):
             nums1[i]=nums2[i-m]
         nums1.sort()
