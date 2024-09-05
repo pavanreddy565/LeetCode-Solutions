b4b91fe3aea37aca76c149110daa70be5b750afd
Pavan Reddy Satti <118699787+pavanreddy565@users.noreply.github.com>
2024-09-05 17:17:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    void combo(int ind,int[] nums,List<Integer>ds,List<List<Integer>>ansList){
+        ansList.add(new ArrayList<>(ds)); 
+        for(int i = ind;i<nums.length;i++) {
+            if(i!=ind && nums[i] == nums[i-1]) continue; 
+            ds.add(nums[i]); 
+            combo(i+1, nums, ds, ansList); 
+            ds.remove(ds.size() - 1);
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans= new ArrayList<>();
+        List<Integer> out = new ArrayList<>();
+        combo(0,nums,out,ans);
+        return ans;
+
+    }
+}
