4453542b4d9bb1a63e8607060e6452f3203b91f3
Pavan Reddy Satti <118699787+pavanreddy565@users.noreply.github.com>
2024-10-16 20:34:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=0;i<asteroids.length;i++){
+            if(st.empty()){
+                st.push(asteroids[i]);
+                continue;
+            }
+            int re=0;
+            while(!st.empty() && st.peek()>0 &&asteroids[i]<0  && Math.abs(asteroids[i])>=Math.abs(st.peek())){
+                
+                re=st.pop();
+                if(Math.abs(re)==Math.abs(asteroids[i]))break;
+            }
+            //System.out.println(re);
+            if(!st.empty()&&(st.peek()<=asteroids[i]||st.peek()*asteroids[i]>0)&&Math.abs(re)!=Math.abs(asteroids[i])){
+                st.push(asteroids[i]);
+            }
+            
+        }
+        int [] ans= new int[st.size()];
+        int i=st.size()-1;
+            while(!st.empty()){
+                ans[i--]=st.pop();
+            }return ans;
+    }
+}
