1070aae9e74409850ba7f1ce89d79aa22d62c987
Pavan Reddy Satti <118699787+pavanreddy565@users.noreply.github.com>
2024-11-27 08:51:41 +0530

    Time: 0 ms (100%), Space: 40.3 MB (50.65%) - LeetHub

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/1406-subtract-the-product-and-sum-of-digits-of-an-integer.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int prod=1, sum=0;
+        while(n>0){
+            int rem=n%10;
+            sum+=rem;
+            prod*=rem;
+            n=n/10;
+        }return prod - sum;
+    }
+}
